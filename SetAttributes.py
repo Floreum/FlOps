d4a674fc8d613f2a22819ede7686822d76fceb62
floreum <nokols@gmail.com>
2025-02-02 05:23:00 -0700

    Fixed something, still not implemented into addon

--- SetAttributes.py
+++ SetAttributes.py
@@ -33,15 +33,23 @@ class DATA_PT_my_attributes(bpy.types.Panel):
         layout = self.layout
 
         # Place the float box and the button on the same row.
+        layout.label(text="Set Attributes")
         row = layout.row(align=True)
         row.prop(context.scene, "my_float_value", text="")
         row.operator("mesh.attribute_set_operator", text="Set")
 
+# Registration
+classes = [MESH_OT_attribute_set, DATA_PT_my_attributes]
 
 def register():
-     bpy.types.DATA_PT_mesh_attributes.append(DATA_PT_my_attributes.draw)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.DATA_PT_mesh_attributes.append(DATA_PT_my_attributes.draw)
 
 def unregister():
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
+    del bpy.types.Scene.my_float_value
     bpy.types.DATA_PT_mesh_attributes.remove(DATA_PT_my_attributes.draw)
 
 if __name__ == "__main__":
