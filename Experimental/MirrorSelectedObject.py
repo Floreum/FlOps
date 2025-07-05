de4877647f3f704a626849a4526350c4d338e82f
Floreum <nokols@gmail.com>
2025-07-04 21:38:49 -0600

    - now case insensitive

--- Experimental/MirrorSelectedObject.py
+++ Experimental/MirrorSelectedObject.py
@@ -7,23 +7,27 @@ def mirror_opposite_object():
         print("No object selected.")
         return
 
-    # Determine mirrored name
     name = obj.name
-    if "_L" in name:
-        mirror_name = name.replace("_L", "_R")
-    elif ".L" in name:
-        mirror_name = name.replace(".L", ".R")
+    mirror_name = None
+
+    if "_l" in name.lower():
+        mirror_name = name[: -2] + ("_R" if name[-2:].islower() else "_R")
+    elif ".l" in name.lower():
+        mirror_name = name[: -2] + (".R" if name[-2:].islower() else ".R")
+    elif "_r" in name.lower():
+        mirror_name = name[: -2] + ("_L" if name[-2:].islower() else "_L")
+    elif ".r" in name.lower():
+        mirror_name = name[: -2] + (".L" if name[-2:].islower() else ".L")
     else:
-        print("Selected object does not have _L or .L in the name.")
+        print("Selected object name does not contain _L/.L/_R/.R")
         return
 
-    # Find the mirrored object
     mirror_obj = bpy.data.objects.get(mirror_name)
     if not mirror_obj:
         print(f"No object found with name '{mirror_name}'")
         return
 
-    # Mirror the location across the X axis (world space)
+    # Mirror the location of the selected object across the X axis
     mirrored_location = Vector(obj.location)
     mirrored_location.x *= -1
     mirror_obj.location = mirrored_location
